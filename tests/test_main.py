30420513400b59c10a0071f15723c6f7111264a5
Luciano Filho <lvgalvaofilho@gmail.com>
2023-03-07 04:04:14 -0300

    add status_code test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,23 @@
 from fastapiproject.main import app
 from fastapi.testclient import TestClient
+from random import randrange
 
 client = TestClient(app)
 
 def test_index():
-    response = client.get('/')
+    response = client.get('/hello')
     assert response.status_code == 200
     assert response.json() == {'msg':'Hello world'}
 
-def test_get_blog():
-    id_blog = '14'
+def test_get_blog_less_than_5():
+    id_blog = randrange(1,4)
     response = client.get(f'/blog/{id_blog}')
     assert response.status_code == 200
+
+def test_get_blog_greater_than_5():
+    id_blog = randrange(6,10)
+    response = client.get(f'/blog/{id_blog}')
+    assert response.status_code == 404
     
     # Try to create a test to check if a not int type causes crash
     # assert response.json() == {'msg': f'Blog with {id_blog}'}
